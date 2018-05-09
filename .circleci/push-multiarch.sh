8f2094366b1edc8c3c356a8444bb42631fd97cea
Jesse Stuart <hi@jessestuart.com>
2018-05-09 19:37:22 -0400

    [commit message skipped]

--- .circleci/push-multiarch.sh
+++ .circleci/push-multiarch.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -euo pipefail
 
+cd $GOPATH/src/github.com/${GITHUB_REPO}
+
+# ============
+# <qemu-support>
 if [ $GOARCH == 'amd64' ]; then
   touch qemu-amd64-static
 else
@@ -8,22 +12,18 @@ else
   curl -sL https://github.com/multiarch/qemu-user-static/releases/download/${QEMU_VERSION}/qemu-${QEMU_ARCH}-static.tar.gz | tar xz
   docker run --rm --privileged multiarch/qemu-user-static:register
 fi
+# </qemu-support>
+# ============
 
-export VERSION_TAG="${REGISTRY}/${IMAGE}:${VERSION}-${TAG}"
-export LATEST_TAG="${REGISTRY}/${IMAGE}:latest-${TAG}"
-
-docker build -t $VERSION_TAG \
-  --build-arg target=$TARGET \
-  --build-arg arch=$QEMU_ARCH \
-  .
-
-echo "Logging in to Docker Hub..."
+# Replace the repo's Dockerfile with our own.
+cp -f $DIR/Dockerfile .
+export IMAGE_ID="${REGISTRY}/${IMAGE}:${VERSION}-${TAG}"
+docker build -t ${IMAGE_ID} --build-arg target=$TARGET --build-arg arch=$QEMU_ARCH .
+# Login to Docker Hub.
 echo $DOCKERHUB_PASS | docker login -u $DOCKERHUB_USER --password-stdin
-echo "Pushing image with tag: $VERSION_TAG"
-docker push $VERSION_TAG
-
-if [ $CIRCLE_BRANCH == 'master' ]; then
-  echo "Build is on master branch -- pushing to 'latest' tag."
-  docker tag $VERSION_TAG $LATEST_TAG
-  docker push $LATEST_TAG
+# Push push push
+docker push ${IMAGE_ID}
+if [ "${CIRCLE_BRANCH}" == 'master' ]; then
+  docker tag  "${IMAGE_ID}" "${REGISTRY}/${IMAGE}:latest-${TAG}";
+  docker push               "${REGISTRY}/${IMAGE}:latest-${TAG}";
 fi
