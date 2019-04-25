7ab572588b585d1f5b4f87f4772749ae62f2686d
Jesse Stuart <hi@jessestuart.com>
2019-04-25 17:26:53 -0400

    feat(deps): Bump qemu to v4.
    Signed-off-by: Jesse Stuart <hi@jessestuart.com>

--- .circleci/load_env.sh
+++ .circleci/load_env.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
-echo 'export GITHUB_REPO=minio/minio' >>$BASH_ENV
-echo 'export GOPATH=/go' >>$BASH_ENV
-echo 'export GOROOT=/usr/local/go' >>$BASH_ENV
-echo 'export IMAGE=minio' >>$BASH_ENV
-echo 'export REGISTRY=jessestuart' >>$BASH_ENV
+echo '
+export GITHUB_REPO=minio/minio
+export GOPATH=/go
+export GOROOT=/usr/local/go
+export IMAGE=minio
+export REGISTRY=jessestuart
 
-echo 'export VERSION=$(curl -s https://api.github.com/repos/${GITHUB_REPO}/releases/latest | jq -r ".tag_name")' >>$BASH_ENV
-echo 'export IMAGE_ID="${REGISTRY}/${IMAGE}:${VERSION}-${TAG}"' >>$BASH_ENV
-echo 'export DIR=`pwd`' >>$BASH_ENV
-echo 'export QEMU_VERSION="v3.1.0-3"' >>$BASH_ENV
+export VERSION=$(curl -s https://api.github.com/repos/${GITHUB_REPO}/releases/latest | jq -r ".tag_name")
+export IMAGE_ID="${REGISTRY}/${IMAGE}:${VERSION}-${TAG}"
+export DIR=`pwd`
+export QEMU_VERSION="v4.0.0"
+' >>$BASH_ENV
 
 . $BASH_ENV
